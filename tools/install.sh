635f446dad08c7789800914f75ab654d1d41d349
Dang Mai <contact@dangmai.net>
2013-05-18 11:03:22 -0400

    Don't automatically change shell
    oh-my-zsh has troubles changing shells in Ubuntu and Debian, and I'd much rather just change it beforehand manually

--- tools/install.sh
+++ tools/install.sh
@@ -23,9 +23,6 @@ cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
 echo "export PATH=\$PATH:$PATH" >> ~/.zshrc
 
-echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
-chsh -s `which zsh`
-
 echo "\033[0;32m"'         __                                     __   '"\033[0m"
 echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
 echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
@@ -34,5 +31,3 @@ echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m
 echo "\033[0;32m"'                        /____/                       '"\033[0m"
 
 echo "\n\n \033[0;32m....is now installed.\033[0m"
-/usr/bin/env zsh
-source ~/.zshrc
